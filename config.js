46f60146a8c4f2431eb8e93e89f88e167042bec9
konghawjien <konghawjien@gmail.com>
2025-08-09 13:51:54 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+// Configuration variables
+
+module.exports = {
+  network: "testnet", // or "mainnet"
+  apiKey: process.env.API_KEY || "",
+  deployPath: "./build",
+  // Add other config variables here
+};
